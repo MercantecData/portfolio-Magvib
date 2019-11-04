0d65cbae2800e48e50342af35ebf6506028e75a6
Magnus <Magnus@p13.dk>
2019-11-04 10:22:54 +0100

    Return Array List: hvis man giver det 4, returnerer det

---
+++ ReturnArrayList/ReturnArrayList/ArrayReturner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ReturnArrayList
+{
+    public class ArrayReturner
+    {
+        public int tal;
+
+        public ArrayReturner(int tal)
+        {
+            this.tal = tal;
+        }
+
+        static void ArrayReturn(int tal)
+        {
+            int[] array = new int[tal];
+            int i = 0;
+            while(i < tal)
+            {
+                array[tal] = i;
+            }
+            Console.WriteLine(array);
+        }
+    }
+}
