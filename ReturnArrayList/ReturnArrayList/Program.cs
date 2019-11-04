80c4cc67723893bc6b98b9f6e2938c1330557a31
Magnus <Magnus@p13.dk>
2019-11-04 12:17:50 +0100

    All Projects with git ignore

---
+++ ReturnArrayList/ReturnArrayList/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ReturnArrayList
+{
+    class Program
+    {
+        static int[] ArrayRetuner(int tal)
+        {
+            int[] arrayCounter = new int[tal];
+            int i = 0;
+            while (i < arrayCounter.Length)
+            {
+                arrayCounter[i] = i+1;
+                i += 1;
+            }
+            return arrayCounter;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Counting to the number you choose!");
+            var CounterTal = Console.ReadLine();
+            var arrayet = ArrayRetuner(Convert.ToInt32(CounterTal));
+            foreach (int i in arrayet)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
---
+++ dataTypes/dataTypes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace dataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string stringVar = "Hello World!!";
+
+            // 32bit: 2,147,483,647 / 64bit: 9,223,372,036,854,775,807
+            int intVar = 100;
+
+            int i = 21474836470;
+
+            // 32bit: 3.402823e38
+            float floatVar = 10.2f;
+
+            // 64bit: 1.79769313486232e308
+            double doubleVar = 12.3d;
+
+            // En character
+            char charVar = 'A';
+
+            // 8bit: True / False
+            bool boolVar = true;
+
+
+            var varInt = 3d;
+            var varString = "string";
+
+
+            Console.WriteLine(stringVar);
+            Console.WriteLine(intVar);
+            Console.WriteLine(floatVar);
+            Console.WriteLine(doubleVar);
+            Console.WriteLine(charVar);
+            Console.WriteLine(boolVar);
+            Console.WriteLine(varInt);
+            Console.WriteLine(varString);
+        }
+    }
+}
