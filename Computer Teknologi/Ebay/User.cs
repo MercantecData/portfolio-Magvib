ad1b2bf4817dc30e71c3ec1550889db5eea9e0cc
Magnus <Magnus@p13.dk>
2019-12-17 21:19:16 +0100

    Renamed Ebay to Computer Teknologi : Fixes #6

---
+++ Computer Teknologi/Ebay/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Ebay
+{
+    public class User
+    {
+        public int id;
+        public string name;
+        private string password;
+
+        public bool CheckPassword(string P)
+        {
+            if (P == password)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public User(int id, string name, string password)
+        {
+            this.id = id;
+            this.name = name;
+            this.password = password;
+        }
+    }
+}
