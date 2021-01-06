c82a83fda48d6a2d5e5037910b7940a44f942d8a
Magnus Nielsen <Magnus@p13.dk>
2021-01-06 13:45:01 +0100

    Overloading

---
+++ H2/Overloading/Overloading/Math.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace Overloading
+{
+    public class M
+    {
+        // Plus
+        public int Plus(int x, int y)
+        {
+            return x + y;
+        }
+        public double Plus(double x, double y)
+        {
+            return x + y;
+        }
+        public double Plus(double x, int y)
+        {
+            return x + y;
+        }
+        public double Plus(int x, double y)
+        {
+            return x + y;
+        }
+        public double Plus(string x, string y)
+        {
+            return Int32.Parse(x) + Int32.Parse(y);
+        }
+
+        // Minus
+        public int Minus(int x, int y)
+        {
+            return x - y;
+        }
+        public double Minus(double x, double y)
+        {
+            return x - y;
+        }
+        public double Minus(double x, int y)
+        {
+            return x - y;
+        }
+        public double Minus(int x, double y)
+        {
+            return x - y;
+        }
+        public double Minus(string x, string y)
+        {
+            return Int32.Parse(x) - Int32.Parse(y);
+        }
+
+        // Gange
+        public int Gange(int x, int y)
+        {
+            return x * y;
+        }
+        public double Gange(double x, double y)
+        {
+            return x * y;
+        }
+        public double Gange(double x, int y)
+        {
+            return x * y;
+        }
+        public double Gange(int x, double y)
+        {
+            return x * y;
+        }
+        public double Gange(string x, string y)
+        {
+            return Int32.Parse(x) * Int32.Parse(y);
+        }
+
+        // Dividere
+        public int Dividere(int x, int y)
+        {
+            return x / y;
+        }
+        public double Dividere(double x, double y)
+        {
+            return x / y;
+        }
+        public double Dividere(double x, int y)
+        {
+            return x / y;
+        }
+        public double Dividere(int x, double y)
+        {
+            return x / y;
+        }
+        public double Dividere(string x, string y)
+        {
+            return Int32.Parse(x) / Int32.Parse(y);
+        }
+
+        // Kvadratrod
+        public int Kvadratrod(int x)
+        {
+            return (int)Math.Sqrt(x);
+        }
+
+        public double Kvadratrod(double x)
+        {
+            return (double)Math.Sqrt(x);
+        }
+        public double Kvadratrod(string x)
+        {
+            return Int32.Parse(x);
+        }
+
+        // Potens
+        public int Potens(int x, int y)
+        {
+            return (int)Math.Pow(x, y);
+        }
+
+        public double Potens(double x, double y)
+        {
+            return (double)Math.Pow(x, y);
+        }
+
+        public double Potens(int x, double y)
+        {
+            return (double)Math.Pow(x, y);
+        }
+
+        public double Potens(double x, int y)
+        {
+            return (double)Math.Pow(x, y);
+        }
+
+        public double Potens(string x, string y)
+        {
+            return (double)Math.Pow(Int32.Parse(x), Int32.Parse(y));
+        }
+    }
+}
---
+++ H2/Overloading/Overloading_TERMINAL/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Overloading;
+
+namespace Overloading_TERMINAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            M m = new M();
+
+            Console.WriteLine(m.Plus(1, 1));
+            Console.WriteLine(m.Plus(1.9, 1.9));
+            Console.WriteLine(m.Plus(1.9, 1));
+            Console.WriteLine(m.Plus(1, 1.9));
+
+            Console.WriteLine(m.Minus(1, 1));
+            Console.WriteLine(m.Minus(1.9, 1.9));
+            Console.WriteLine(m.Minus(1.9, 1));
+            Console.WriteLine(m.Minus(1, 1.9));
+
+            Console.WriteLine(m.Dividere(1, 1));
+            Console.WriteLine(m.Dividere(1.9, 1.9));
+            Console.WriteLine(m.Dividere(1.9, 1));
+            Console.WriteLine(m.Dividere(1, 1.9));
+
+            Console.WriteLine(m.Gange(1, 1));
+            Console.WriteLine(m.Gange(1.9, 1.9));
+            Console.WriteLine(m.Gange(1.9, 1));
+            Console.WriteLine(m.Gange(1, 1.9));
+
+            Console.WriteLine(m.Kvadratrod(20));
+            Console.WriteLine(m.Kvadratrod(20.5));
+
+
+            Console.WriteLine(m.Potens(1, 1));
+            Console.WriteLine(m.Potens(1.9, 1.9));
+            Console.WriteLine(m.Potens(1.9, 1));
+            Console.WriteLine(m.Potens(1, 1.9));
+
+        }
+    }
+}
