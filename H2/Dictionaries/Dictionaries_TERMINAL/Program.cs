cab41ddea94eb5a1cac826a1757ae3fe99fa65c8
Magnus Nielsen <Magnus@p13.dk>
2021-01-07 11:59:29 +0100

    Render all books

--- H2/Dictionaries/Dictionaries_TERMINAL/Program.cs
+++ H2/Dictionaries/Dictionaries_TERMINAL/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Dictionaries;
 
 namespace Dictionaries_TERMINAL
@@ -12,6 +13,9 @@ namespace Dictionaries_TERMINAL
             l.addBook(new Book("Harry Potter E1", 374));
             l.addBook(new Book("Harry Potter E2", 423));
             l.addBook(new Book("Harry Potter E3", 873));
+            l.addBook(new Book("Harry Potter E4", 374));
+            l.addBook(new Book("Harry Potter E5", 423));
+            l.addBook(new Book("Harry Potter E6", 873));
 
             l.removeBookByName("Harry Potter E1");
             l.removeBook(l.getBook("Harry Potter E2"));
@@ -20,9 +24,16 @@ namespace Dictionaries_TERMINAL
 
             Console.WriteLine(l.getBook("Harry Potter E3").totalPages);
 
-            l.getBook("Harry Potter E3").changeName("Harry Potter E4", l);
+            l.getBook("Harry Potter E3").changeName("Harry Potter E7", l);
 
             Console.WriteLine(l.getBook("Harry Potter E4").name);
+
+            Console.WriteLine("----------");
+
+            foreach (KeyValuePair<string, Book> b in l.books)
+            {
+                Console.WriteLine(b.Key);
+            }
         }
     }
 }
