5c6c780ea686e362dea6aec26e56e459618c56c0
Magnus Nielsen <Magnus@p13.dk>
2021-01-11 14:13:33 +0100

    RoleManagment

---
+++ H2/OOP Projekt/RoleManagment/RoleManagment/MySql.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace RoleManagment
+{
+    public abstract class MySql
+    {
+        public int id { get; set; }
+        
+        public string db_host { get; private set; }
+        public int db_port { get; private set; }
+        public string db_name { get; private set; }
+
+        public string db_user { get; private set; }
+        public string db_pass { get; private set; }
+
+        MySqlConnection con;
+
+        public MySql(int id = 0, string db_host = "os.ht", int db_port = 3306, string db_name = "dev", string db_user = "test", string db_pass = "8e607a4752fa2e59413e5790536f2b42")
+        {
+            this.id = id;
+            this.db_host = db_host;
+            this.db_port = db_port;
+            this.db_name = db_name;
+            this.db_user = db_user;
+            this.db_pass = db_pass;
+
+            string cs = "server=" + this.db_host + ";user=" + this.db_user + ";database=" + this.db_name + ";port=" + this.db_port + ";password=" + this.db_pass;
+
+            try {
+                this.con = new MySqlConnection(cs);
+                this.con.Open();
+            } catch (Exception)
+            {
+                Console.WriteLine("Can't connect to db");
+            }
+        }
+
+        public abstract void save();
+
+        public abstract void delete();
+
+        public int getLastId()
+        {
+            var id = this.get("SELECT LAST_INSERT_ID();");
+            while (id.Read())
+            {
+                return (int)id.GetInt32(0);
+            }
+
+            return 0;
+        }
+
+        public MySqlDataReader get(string sql)
+        {
+            MySqlDataReader rdr = new MySqlCommand(sql, this.con).ExecuteReader();
+
+            return rdr;
+        }
+
+        public void set(string sql)
+        {
+            new MySqlCommand(sql, this.con).ExecuteScalar();
+        }
+    }
+}
---
+++ H2/OOP Projekt/RoleManagment/RoleManagment/User.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RoleManagment
+{
+    public class User : MySql
+    {
+        public string username { get; set; }
+        public string password { get; private set; }
+        public Role role { get; private set; }
+
+        public User(int id = 0) : base()
+        {
+            try
+            {
+                var user = this.get("SELECT * FROM user WHERE id = " + (int)id + " LIMIT 1");
+
+                while (user.Read())
+                {
+                    this.id = user.GetInt32(0);
+                    this.username = user.GetString(1);
+                    this.password= user.GetString(2);
+                    this.role = new Role(user.GetInt32(3));
+
+                }
+                user.Close();
+            } catch (Exception)
+            {
+            }
+
+            if(this.id == 0)
+            {
+                this.id = 0;
+                this.role = new Role(0);
+            }
+        }
+
+        public User(string username) : base()
+        {
+            try
+            {
+                var user = this.get("SELECT * FROM user WHERE username = '" + username + "' LIMIT 1");
+
+                while (user.Read())
+                {
+                    this.id = user.GetInt32(0);
+                    this.username = user.GetString(1);
+                    this.password = user.GetString(2);
+                    this.role = new Role(user.GetInt32(3));
+                }
+                user.Close();
+            }
+            catch (Exception)
+            {
+            }
+
+            if (this.id == 0)
+            {
+                this.id = 0;
+                this.role = new Role(0);
+            }
+        }
+
+        public override void save()
+        {
+            if(this.id != 0)
+            {
+                try
+                {
+                    this.set("UPDATE user SET username = '"+this.username+"', password = '"+this.password+"', role = " + this.role.id +" WHERE id = " + this.id + "");
+                } catch (Exception)
+                {
+                    throw new Exception("Failed to save user");
+                }
+            } else
+            {
+                try
+                {
+                    this.set("INSERT INTO user(username, password, role) VALUES ('"+this.username+"', '" + this.password + "', " + this.role.id + ")");
+                    this.id = this.getLastId();
+                }
+                catch (Exception)
+                {
+                    throw new Exception("Failed to insert user");
+                }
+            }
+        }
+
+        public override void delete()
+        {
+            if (this.id != 0)
+            {
+                try
+                {
+                    this.set("DELETE FROM user WHERE id = '" + this.id + "'");
+                }
+                catch (Exception)
+                {
+                    throw new Exception("Failed to delete user");
+                }
+            }
+        }
+
+        public bool checkPassword(string pass)
+        {
+
+            if (this.password == this.encryptPassword(pass))
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        public string encryptPassword(string pass, bool setPassword = false)
+        {
+            byte[] encodedPassword = new UTF8Encoding().GetBytes(pass);
+            byte[] hash = ((HashAlgorithm)CryptoConfig.CreateFromName("MD5")).ComputeHash(encodedPassword);
+            string encoded = BitConverter.ToString(hash).Replace("-", string.Empty).ToLower();
+
+            if(setPassword)
+            {
+                this.password = encoded;
+            }
+
+            return encoded;
+        }
+    }
+}
